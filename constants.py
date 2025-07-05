5c3fc628c7ded3980c8986f7d7021e9ccc165069
Linus Claußen <claussen@ibr.cs.tu-bs.de>
2025-07-05 18:33:09 +0200

    init

---
+++ constants.py
@@ -0,0 +1,10 @@
+# --- ACTION REQUIRED: CONSTANT VALUES THAT NEED TO BE ADJUSTED ---
+
+# Enter the exact names (in caps) of players you want to log:
+KNOWN_PLAYERS = ["RETRAC", "JISOO", "SAMPHIL"]
+
+# download-url of your google sheet:
+url = "https://drive.google.com/uc?export=download&id=1dMJ5nniCYicuOyf6JYXhNymbn7yikGuV"
+
+# names as displayed in plots
+players = ["Bobo", "Phil", "Steven"]
